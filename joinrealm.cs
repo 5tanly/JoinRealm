689b1346dfc70dabd55caece5fd990b05bfb86bd
5tanly <31938727+5tanly@users.noreply.github.com>
2020-05-12 10:51:02 -0400

    Switch from threading to Update()

--- joinrealm.cs
+++ joinrealm.cs
@@ -8,9 +8,8 @@ public class JoinRealm : ChatBot{
 
   string enabled = string.Empty;
   string realm = string.Empty;
-  // string username = string.Empty;
-  private Thread joinThread;
-  private bool _joinedRealm;
+  private bool _joinedRealm = true; //Set to true by default so when loaded inside a realm it won't start spamming until hub is joined
+  int count = 0;
 
   public void GetSettings(){
     //Get user defined settings from joinrealm.ini
@@ -19,31 +18,13 @@ public class JoinRealm : ChatBot{
     realm = Lines[2].Remove(0,6).ToLower();
   }
 
-  public void joinLoop(){
-    while(true){
-      if (_joinedRealm == false){
-        //Checks if _joinedRealm is true or false and starts spamming "/server" if _joinedRealm is false
-        SendText("/server "+ realm);
-        Thread.Sleep(1000);
-      }
-      else{
-        //Stops the loop from continuing to run in the background
-        break;
-      }
-    }
-  }
-
-  public void createThread(){
+  public void startLoop(){
     //Set variables and start the loop
-    LogToConsole("Starting loop");
     _joinedRealm = false;
-    joinThread  = new Thread(joinLoop);
-    joinThread.Start();
   }
 
-  public void killThread(){
+  public void stopLoop(){
     //Set variables to stop the loop
-    LogToConsole("Stopping loop");
     _joinedRealm = true;
   }
 
@@ -53,9 +34,8 @@ public class JoinRealm : ChatBot{
       LogToConsole("Sucessfully Initialized!");
       LogToConsole("Enabled: "+enabled);
       LogToConsole("Realm: "+realm);
-      // username = GetUsername();
       //Start the loop that sends "/server"
-      createThread();
+      startLoop();
     }
     else{
       //Print to console if the script settings are disabled
@@ -67,23 +47,30 @@ public class JoinRealm : ChatBot{
     }
   }
 
+  public override void Update(){
+    if (enabled == "true"){
+      if (count > 10 && _joinedRealm == false){
+        //Checks if _joinedRealm is true or false and starts spamming "/server" if _joinedRealm is false
+        count = 0;
+        SendText("/server "+ realm);
+        // Console.WriteLine("SENDING /SERVER");
+      }
+      else if (_joinedRealm == true){
+        //pass
+      }
+      else count++;
+    }
+  }
+
   public override void GetText(string text){
     text = GetVerbatim(text);
     if (text =="(!) While you were offline…"){
       //Stop spamming "/server" if the player has already joined the realm.
-      LogToConsole("Realm joined!");
-      killThread();
+      stopLoop();
     }
     else if (text == "(!) You have teleported to Hub Realm!"){
       //Start spamming "/server" if the player has teleported back to hub realm.
-      LogToConsole("Hub joined!");
-      createThread();
+      startLoop();
     }
-    // else if (text.Contains("[me -> " + username + "] unload") && text[0] == '['){
-    //   //Unload script if you message yourself: (/m {USERNAME} unload joinrealm)
-    //   killThread();
-    //   LogToConsole("Unloading script");
-    //   UnloadBot();
-    // }
   }
 }
